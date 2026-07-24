0ad974c143a4961cc68ee19fd8b71ced285ce3b0
Ethan Morgan <ethan@gweithio.com>
2026-07-24 22:28:36 +0100

    more ghostty stuff

--- home/.config/nvim/lua/sixfourtwelve/lazy.lua
+++ home/.config/nvim/lua/sixfourtwelve/lazy.lua
@@ -133,8 +133,8 @@ return require("lazy").setup({
           floats = "transparent",
         },
       })
-      -- vim.cmd("colorscheme catppuccin-mocha")
-      vim.cmd("colorscheme tokyonight-moon")
+      vim.cmd("colorscheme catppuccin-mocha")
+      -- vim.cmd("colorscheme tokyonight-moon")
       vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
       vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
       vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
